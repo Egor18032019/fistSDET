f3653badffb6464dcc4ccc1fa4774cef4af27f42
Egor <goro4@mail.ru>
2023-02-25 22:04:15 +0500

    for design

--- src/test/java/pages/ProductsPage.java
+++ src/test/java/pages/ProductsPage.java
@@ -3,7 +3,7 @@ package pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import pages.PageBase;
+import pages.base.PageBase;
 
 public class ProductsPage extends PageBase {
 
