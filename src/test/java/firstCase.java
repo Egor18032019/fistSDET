5b9a5d60a7cef7b57abfa12c2a95a16c31065c91
Egor <goro4@mail.ru>
2023-02-25 16:42:15 +0500

    мелкие правки

--- src/test/java/firstCase.java
+++ src/test/java/firstCase.java
@@ -11,48 +11,57 @@ public class firstCase extends TestBase {
     CompletePage completePage;
     String username = "standard_user";
     String password = "secret_sauce";
+    boolean canNextTest = true;
 
     @Test(priority = 1, alwaysRun = true)
     public void userCanLoginSuccessfully() {
         loginPage = new LoginPage(driver);
         loginPage.UserLogin(username, password);
-
+        String url = driver.getCurrentUrl();
+        Assert.assertEquals(url, "https://www.saucedemo.com/inventory.html");
     }
-//TODO если предыдущий не выполнился следующий не запускается
-    @Test(priority = 2, alwaysRun = false)
+
+    //TODO вынести все урлы в отдельный енум ? класс?
+    @Test(priority = 2, alwaysRun = false, dependsOnMethods = {"userCanLoginSuccessfully"})
     public void chooseProduct() {
         productsPage = new ProductsPage(driver);
         System.out.println("продукт ");
         productsPage.addInCart();
         System.out.println("добавили в корзину");
         productsPage.goToCart();
-
+        String url = driver.getCurrentUrl();
+        Assert.assertEquals(url, "https://www.saucedemo.com/cart.html");
     }
 
-    @Test(priority = 3)
+    @Test(priority = 3, dependsOnMethods = {"chooseProduct"})
     public void cartClickCheckout() {
         cartPage = new CartPage(driver);
         cartPage.clickCheckout();
+        String url = driver.getCurrentUrl();
+        Assert.assertEquals(url, "https://www.saucedemo.com/checkout-step-one.html");
     }
 
-    @Test(priority = 4)
+    @Test(priority = 4, dependsOnMethods = {"cartClickCheckout"})
     public void fillCheckout() {
         checkoutPage = new CheckoutPage(driver);
         checkoutPage.checkoutStep("test", "test", "test");
+        String url = driver.getCurrentUrl();
+        Assert.assertEquals(url, "https://www.saucedemo.com/checkout-step-two.html");
     }
 
-    @Test(priority = 5)
+    @Test(priority = 5, dependsOnMethods = {"fillCheckout"})
     public void overviewClickFinish() {
         overviewPage = new OverviewPage(driver);
         overviewPage.clickFinish();
+        String url = driver.getCurrentUrl();
+        Assert.assertEquals(url, "https://www.saucedemo.com/checkout-complete.html");
     }
 
-    @Test(priority = 6)
+    @Test(priority = 6, dependsOnMethods = {"overviewClickFinish"})
     public void checkComplete() {
         completePage = new CompletePage(driver);
         String url = driver.getCurrentUrl();
         Assert.assertEquals(url, "https://www.saucedemo.com/checkout-complete.html");
-
         Assert.assertTrue(completePage.successPurchase.getText().contains("THANK YOU FOR YOUR ORDER"));
     }
 }
