d842655040df63a853f8e761499310dbb24741e7
Egor <goro4@mail.ru>
2023-02-26 13:54:28 +0500

    all done

--- src/test/java/cases/TestCase.java
+++ src/test/java/cases/TestCase.java
@@ -15,6 +15,7 @@ public class TestCase extends BaseCase {
     String username = "standard_user";
     String password = "secret_sauce";
 
+//    @DisplayName()
     @Test(priority = 1, alwaysRun = true)
     public void userCanLoginSuccessfully() {
         loginPage = new LoginPage(BaseCase.driver);
@@ -24,7 +25,7 @@ public class TestCase extends BaseCase {
     }
 
     //TODO вынести все урлы в отдельный енум ? класс?
-    @Test(priority = 2, alwaysRun = false, dependsOnMethods = {"userCanLoginSuccessfully"} ,groups = "fistCase")
+    @Test(priority = 2, alwaysRun = false, dependsOnMethods = {"userCanLoginSuccessfully"}, groups = "fistCase")
     public void chooseProduct() {
         productsPage = new ProductsPage(BaseCase.driver);
         System.out.println("продукт ");
@@ -35,7 +36,7 @@ public class TestCase extends BaseCase {
         Assert.assertEquals(url, "https://www.saucedemo.com/cart.html");
     }
 
-    @Test(priority = 3, dependsOnMethods = {"chooseProduct"},groups = "fistCase")
+    @Test(priority = 3, dependsOnMethods = {"chooseProduct"}, groups = "fistCase")
     public void cartClickCheckout() {
         cartPage = new CartPage(BaseCase.driver);
         cartPage.clickCheckout();
@@ -43,7 +44,7 @@ public class TestCase extends BaseCase {
         Assert.assertEquals(url, "https://www.saucedemo.com/checkout-step-one.html");
     }
 
-    @Test(priority = 4, dependsOnMethods = {"cartClickCheckout"},groups = "fistCase")
+    @Test(priority = 4, dependsOnMethods = {"cartClickCheckout"}, groups = "fistCase")
     public void fillCheckout() {
         checkoutPage = new CheckoutPage(BaseCase.driver);
         checkoutPage.checkoutStep("test", "test", "test");
@@ -51,7 +52,7 @@ public class TestCase extends BaseCase {
         Assert.assertEquals(url, "https://www.saucedemo.com/checkout-step-two.html");
     }
 
-    @Test(priority = 5, dependsOnMethods = {"fillCheckout"},groups = "fistCase")
+    @Test(priority = 5, dependsOnMethods = {"fillCheckout"}, groups = "fistCase")
     public void overviewClickFinish() {
         overviewPage = new OverviewPage(BaseCase.driver);
         overviewPage.clickFinish();
@@ -59,13 +60,13 @@ public class TestCase extends BaseCase {
         Assert.assertEquals(url, "https://www.saucedemo.com/checkout-complete.html");
     }
 
-    @Test(priority = 6, dependsOnMethods = {"overviewClickFinish"},groups = "fistCase")
+    @Test(priority = 6, dependsOnMethods = {"overviewClickFinish"}, groups = "fistCase")
     public void checkComplete() {
         completePage = new CompletePage(BaseCase.driver);
         String url = BaseCase.driver.getCurrentUrl();
         Assert.assertEquals(url, "https://www.saucedemo.com/checkout-complete.html");
         Assert.assertTrue(completePage.successPurchase.getText().contains("THANK YOU FOR YOUR ORDER"));
-     }
+    }
 
     @Test(priority = 7, alwaysRun = true, groups = "second Case")
     public void userNoCanLoginSuccessfully() {
